65de15643e5eacb586d3c60e5722b6064443d861
Juan Andres <juan@floored.com>
2015-11-07 23:05:47 -0500

    Using distribution app only

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ var app = express();
 // https://scotch.io/tutorials/use-ejs-to-template-your-node-application
 app.set('view engine', 'ejs');
 
-app.use('/static', express.static(__dirname + '/../public'));
+// app.use('/static', express.static(__dirname + '/../public'));
 app.use('/js', express.static(__dirname + '/../dist/js'));
 
 app.get('/', function(req, res) {
