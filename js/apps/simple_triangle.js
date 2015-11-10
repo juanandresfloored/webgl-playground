5f8b03ec2913b1790c973d695379be671667bec0
Juan Andres <juan@floored.com>
2015-11-09 23:11:07 -0500

    Fix simple_triangle program

--- js/apps/simple_triangle.js
+++ js/apps/simple_triangle.js
@@ -8,11 +8,12 @@ var fragmentShaderSource = shaderStore['paint_red.frag'];
 
 var program = util.buildGLProgram(gl, vertexShaderSource, fragmentShaderSource);
 
-var attrPosition = gl.getAttribLocation(program, 'a_position'); // Getting vertex attr reference
-gl.enableVertexAttribArray(attrPosition); // turn on getting data out of a buffer for this attribute
-
 var buffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+
+var attrPosition = gl.getAttribLocation(program, 'a_Position'); // Getting vertex attr reference
+gl.enableVertexAttribArray(attrPosition); // turn on getting data out of a buffer for this attribute
+gl.vertexAttribPointer(attrPosition, 2, gl.FLOAT, false, 0, 0);
 
 var triangleVec2Data = new Float32Array([
   0, 0,
