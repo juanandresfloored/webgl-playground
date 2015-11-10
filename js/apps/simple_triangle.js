e4a5d146af5d9f531adc1d7453980e4b551d96c3
Juan Andres <juan@floored.com>
2015-11-09 22:42:27 -0500

    Add simple_triangle app
    Add Simple_triangle vert shader

---
+++ js/apps/simple_triangle.js
@@ -0,0 +1,25 @@
+var util = require('./../util');
+var shaderStore = require('./../shaders/store');
+
+var canvas = util.buildCanvas(500, 500);
+var gl = canvas.getContext('webgl');
+var vertexShaderSource = shaderStore['simple_triangle.vert'];
+var fragmentShaderSource = shaderStore['paint_red.frag'];
+
+var program = util.buildGLProgram(gl, vertexShaderSource, fragmentShaderSource);
+
+var attrPosition = gl.getAttribLocation(program, 'a_position'); // Getting vertex attr reference
+gl.enableVertexAttribArray(attrPosition); // turn on getting data out of a buffer for this attribute
+
+var buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+
+var triangleVec2Data = new Float32Array([
+  0, 0,
+  1, 0,
+  0, 1
+]);
+
+gl.bufferData(gl.ARRAY_BUFFER, triangleVec2Data, gl.STATIC_DRAW); // geometry doesn't change much
+
+gl.drawArrays(gl.TRIANGLES,  0, 3); // 3 vec2s
