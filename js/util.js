422441c007953691e9e4d6ff0f7eede5161ab121
Juan Andres <juan@floored.com>
2015-11-09 22:42:06 -0500

    Move util functions to util.js

---
+++ js/util.js
@@ -0,0 +1,41 @@
+function compileShader(gl, shader) {
+  gl.compileShader(shader);
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    console.log(gl.getShaderInfoLog(shader));
+    return false;
+  }
+  return true;
+}
+
+function buildShaderFromSource(gl, type, source) {
+  var vertexShader = gl.createShader(type);
+  gl.shaderSource(vertexShader, source);
+  compileShader(gl, vertexShader);
+  return vertexShader;
+}
+
+function buildCanvas(width, height) {
+  var canvas = document.createElement('canvas');
+  canvas.width = width;
+  canvas.height = height;
+  document.body.appendChild(canvas);
+  return canvas;
+}
+function buildGLProgram(gl, vertexShaderSource, fragmentShaderSource) {
+
+  var vertexShader = buildShaderFromSource(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  var fragmentShader = buildShaderFromSource(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  gl.useProgram(program);
+
+  return program;
+}
+
+exports = {
+  buildGLProgram: buildGLProgram,
+  buildCanvas: buildCanvas
+};
