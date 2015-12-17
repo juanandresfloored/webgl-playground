a9f56f7fc73ab959785faa82f4c8ce61ae278300
Juan Andres <juan@floored.com>
2015-12-16 20:51:27 -0500

    Update build/watch apps scripts

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use('/js', express.static(__dirname + '/../dist/js'));
 
 app.get('/', function(req, res) {
-  res.render('index.ejs');
+  res.render('index.ejs', {app_name: req.query.app_name ? req.query.app_name : 'simple_point'});
 });
 
 app.listen(process.env.PORT || 3001);
